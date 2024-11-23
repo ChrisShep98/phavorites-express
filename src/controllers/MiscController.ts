c4db14b99269e3809514f33d8dbcffdb397d9e7f
ChrisShep98 <shepardchristopher1@gmail.com>
2024-11-22 22:07:59 -0500

    remove console log

--- src/controllers/MiscController.ts
+++ src/controllers/MiscController.ts
@@ -3,7 +3,6 @@ const path = require("path");
 
 class MiscController {
   welcomePage = async (req: Request, res: Response) => {
-    console.log(path.join(__dirname, "../..", "public", "index.html"));
     return res.sendFile(path.join(__dirname, "../..", "public", "index.html"));
   };
 }
